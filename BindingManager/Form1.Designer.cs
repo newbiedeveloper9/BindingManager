89dc2591b772b56efd47f9ed20928d579f87dbdb
Michał Wojciechowski <newbiedeveloper9@users.noreply.github.com>
2019-02-02 20:10:50 +0100

    init commit

---
+++ BindingManager/Form1.Designer.cs
@@ -0,0 +1,97 @@
+﻿namespace BindingManager
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.bindingTxtBox = new System.Windows.Forms.TextBox();
+            this.codeTxtBox = new System.Windows.Forms.TextBox();
+            this.generateCode = new System.Windows.Forms.Button();
+            this.templTypesTxtBox = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // bindingTxtBox
+            // 
+            this.bindingTxtBox.Location = new System.Drawing.Point(180, 12);
+            this.bindingTxtBox.Multiline = true;
+            this.bindingTxtBox.Name = "bindingTxtBox";
+            this.bindingTxtBox.Size = new System.Drawing.Size(249, 397);
+            this.bindingTxtBox.TabIndex = 0;
+            // 
+            // codeTxtBox
+            // 
+            this.codeTxtBox.Location = new System.Drawing.Point(435, 12);
+            this.codeTxtBox.Multiline = true;
+            this.codeTxtBox.Name = "codeTxtBox";
+            this.codeTxtBox.ReadOnly = true;
+            this.codeTxtBox.Size = new System.Drawing.Size(557, 426);
+            this.codeTxtBox.TabIndex = 1;
+            // 
+            // generateCode
+            // 
+            this.generateCode.Location = new System.Drawing.Point(354, 415);
+            this.generateCode.Name = "generateCode";
+            this.generateCode.Size = new System.Drawing.Size(75, 23);
+            this.generateCode.TabIndex = 2;
+            this.generateCode.Text = "Generuj";
+            this.generateCode.UseVisualStyleBackColor = true;
+            this.generateCode.Click += new System.EventHandler(this.generateCode_Click);
+            // 
+            // templTypesTxtBox
+            // 
+            this.templTypesTxtBox.Location = new System.Drawing.Point(12, 12);
+            this.templTypesTxtBox.Multiline = true;
+            this.templTypesTxtBox.Name = "templTypesTxtBox";
+            this.templTypesTxtBox.ReadOnly = true;
+            this.templTypesTxtBox.Size = new System.Drawing.Size(162, 397);
+            this.templTypesTxtBox.TabIndex = 3;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1004, 450);
+            this.Controls.Add(this.templTypesTxtBox);
+            this.Controls.Add(this.generateCode);
+            this.Controls.Add(this.codeTxtBox);
+            this.Controls.Add(this.bindingTxtBox);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox bindingTxtBox;
+        private System.Windows.Forms.TextBox codeTxtBox;
+        private System.Windows.Forms.Button generateCode;
+        private System.Windows.Forms.TextBox templTypesTxtBox;
+    }
+}
+
---
+++ BindingManager/Form1.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.IO;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace BindingManager
+{
+    public partial class Form1 : Form
+    {
+        public static readonly string TemplatesPath = "Templates";
+        public static readonly string TemplateExtension = "templ";
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            Directory.CreateDirectory(TemplatesPath);
+            Task.Factory.StartNew(RefreshTemplateTypes);
+        }
+
+        private void generateCode_Click(object sender, EventArgs e)
+        {
+            codeTxtBox.ResetText();
+            foreach (var line in bindingTxtBox.Lines)
+            {
+                if (string.IsNullOrEmpty(line))
+                    continue;
+
+                string varName;
+                string varType;
+
+                if (line.Contains(" "))
+                {
+                    var separatedLine = line.Split(' ');
+                    varName = separatedLine[0];
+                    varType = separatedLine[1];
+                }
+                else
+                {
+                    varName = line;
+                    varType = "default";
+                }
+
+                var template = GetTemplateOrDefault(varType);
+                var code = GenerateCodeByTemplate(template, varName, varType);
+                codeTxtBox.AppendText(code);
+                codeTxtBox.Text += Environment.NewLine + Environment.NewLine;
+            }
+        }
+
+        private string GenerateCodeByTemplate(string templ, string varName, string varType)
+        {
+            var fieldName = GetNameAsField(varName);
+
+            templ = templ.Replace("__field__", fieldName);
+            templ = templ.Replace("__prop__", varName);
+            templ = templ.Replace("__type__", varType);
+
+            return templ;
+        }
+
+        private string GetTemplateOrDefault(string varType) =>
+             File.Exists(GetTemplatePath(varType)) ? File.ReadAllText(GetTemplatePath(varType)) : File.ReadAllText(GetTemplatePath("default"));
+
+        private string GetTemplatePath(string templateName) =>
+            $@"{Path.GetDirectoryName(Application.ExecutablePath)}\{TemplatesPath}\{templateName}.{TemplateExtension}";
+
+        private string GetNameAsField(string varName)
+        {
+            var toLower = varName.ToLower();
+            var afterRemove = varName.Remove(0, 1);
+            return $"_{toLower[0]}{afterRemove}";
+        }
+
+        private string[] GetTemplateTypes()
+        {
+            var files = Directory.GetFiles($@"{Path.GetDirectoryName(Application.ExecutablePath)}\{TemplatesPath}\",
+                 $"*.{TemplateExtension}");
+
+            for (var i = 0; i < files.Length; i++)
+                files[i] = Path.GetFileNameWithoutExtension(files[i]);
+
+            return files;
+        }
+
+        private void RefreshTemplateTypes()
+        {
+            while (true)
+            {
+                ThreadHelperClass.SetText(this, templTypesTxtBox, GetTemplateTypes());
+                Thread.Sleep(1500);
+            }
+        }
+    }
+}
